71141a29427ff7b89aacf660b8d5a6cd8fb0f843
Fabio <fabio@itexto.com.br>
2022-07-01 16:58:57 -0300

    autorização e ajuste necessario security

---
+++ src/main/java/br/com/alura/forum/security/SecurityConfigurations.java
@@ -0,0 +1,30 @@
+package br.com.alura.forum.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    //Configuração de autenticação
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {}
+
+
+    //Configuração de autorização
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/topicos").permitAll()
+                .antMatchers(HttpMethod.GET,"/topicos/*").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin();
+    }
+
+}
